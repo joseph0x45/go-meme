ec5ce6fff21693b15ab335a493565c3bdb81ca16
TheWisePigeon <zozozozeph@gmail.com>
2023-06-23 20:25:57 +0000

    work in progress

--- main.go
+++ main.go
@@ -3,33 +3,29 @@ package main
 import (
 	"bufio"
 	"fmt"
-	// "go-meme/generator"
+	"go-meme/generator"
 	"os"
 	"strconv"
 )
 
 func main(){
     type Template struct{
-        number string
         id string
         name string
         number_of_captions int
     }
     templates := []Template{
         {
-            number: "1",
             id: "61579",
             name: "One does not simply",
             number_of_captions: 2,
         },
         {
-            number: "2",
             id: "112126428",
             name: "Distracted boyfriend",
             number_of_captions: 2,
         },
         {
-            number: "3",
             id: "4087833",
             name: "Waiting skeleton",
             number_of_captions: 2,
@@ -38,8 +34,10 @@ func main(){
     input_reader := bufio.NewScanner(os.Stdin)
     println("Welcome to Gomeme, a goofy meme generator written in the Goofiest language in the world!")
     println("Find below the list of all available templates and chose the one you want\n")
+    i := 1
     for _, value := range templates{
-        fmt.Printf("%s - %s\n", value.number, value.name)
+        fmt.Printf("%d - %s\n", i, value.name)
+        i++
     }
     print("Chose your meme template : ")
     input_reader.Scan()
@@ -53,5 +51,5 @@ func main(){
         println("Invalid template number.")
         return
     }
-    fmt.Printf("You chose %d", chosen_template)
+    generator.Generate()
 }
