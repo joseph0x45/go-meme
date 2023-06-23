76a93271cd465a6555fcbecb13179486395019cb
TheWisePigeon <zozozozeph@gmail.com>
2023-06-23 21:37:33 +0000

    feat: generate meme and get url

--- generator/generator.go
+++ generator/generator.go
@@ -1,20 +1,52 @@
 package generator
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"go-meme/shared"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+)
 
-func Generate(){
-    url := "https://api.imgflip.com/caption_image"
-    data := map[string]interface{}{
-        "template_id":"",
-        "username":"",
-        "password":"",
-        "text0":"one does not simply",
-        "text1":"learn Rust's basics in one night",
-    }
-    response, err := http.Post(
-        url,
-        "application/json",
-        nil,
+func Generate( template shared.Template, text0 string, text1 string ){
+    println("Cooking......")
+    uri := "https://api.imgflip.com/caption_image"
+    form_data := url.Values{}
+    form_data.Set("template_id", template.Id)
+    form_data.Set("username", "pigeondev")
+    form_data.Set("password", "N;QgK~TwDM&jgJ4")
+    form_data.Set("text0", text0)
+    form_data.Set("text1", text1)
+
+    request, err := http.NewRequest(
+        "POST",
+        uri,
+        strings.NewReader(form_data.Encode()),
     )
+    request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+    if err != nil {
+        println("Something went wrong when generating your meme.")
+        println("Please retry or report this bug on https://github.com/TheWisePigeon/go-meme")
+        return
+    }
 
+    response, err := http.DefaultClient.Do(request)
+    if err != nil {
+        println("Something went wrong when generating your meme.")
+        println("Please retry or report this bug on https://github.com/TheWisePigeon/go-meme")
+        return
+    }
+
+    defer response.Body.Close()
+
+    response_body, err := ioutil.ReadAll(response.Body)
+    var response_data map[string]interface{}
+    if err:= json.Unmarshal(response_body, &response_data); err!=nil {
+        println("Something went wrong when generating your meme.")
+        println("Please retry or report this bug on https://github.com/TheWisePigeon/go-meme")
+        return
+    }
+    fmt.Printf("%+v\n", response_data)
 }
--- main.go
+++ main.go
@@ -4,39 +4,32 @@ import (
 	"bufio"
 	"fmt"
 	"go-meme/generator"
+	"go-meme/shared"
 	"os"
 	"strconv"
 )
 
 func main(){
-    type Template struct{
-        id string
-        name string
-        number_of_captions int
-    }
-    templates := []Template{
+    templates := []shared.Template{
         {
-            id: "61579",
-            name: "One does not simply",
-            number_of_captions: 2,
+            Id: "61579",
+            Name: "One does not simply",
         },
         {
-            id: "112126428",
-            name: "Distracted boyfriend",
-            number_of_captions: 2,
+            Id: "112126428",
+            Name: "Distracted boyfriend",
         },
         {
-            id: "4087833",
-            name: "Waiting skeleton",
-            number_of_captions: 2,
+            Id: "4087833",
+            Name: "Waiting skeleton",
         },
     }
     input_reader := bufio.NewScanner(os.Stdin)
     println("Welcome to Gomeme, a goofy meme generator written in the Goofiest language in the world!")
     println("Find below the list of all available templates and chose the one you want\n")
-    i := 1
+    i := 0
     for _, value := range templates{
-        fmt.Printf("%d - %s\n", i, value.name)
+        fmt.Printf("%d - %s\n", i, value.Name)
         i++
     }
     print("Chose your meme template : ")
@@ -47,9 +40,16 @@ func main(){
         println("For example, enter 1 for the 'One does not simply' template")
         return
     }
-    if chosen_template > len(templates) || chosen_template <= 0 {
+    if chosen_template > len(templates)-1 || chosen_template < 0 {
         println("Invalid template number.")
         return
     }
-    generator.Generate()
+    fmt.Printf("You chose the %s\n", templates[chosen_template].Name)
+    print("Enter the top text: ")
+    input_reader.Scan()
+    text0 := input_reader.Text()
+    print("Enter the bottom text: ")
+    input_reader.Scan()
+    text1 := input_reader.Text()
+    generator.Generate(templates[chosen_template], text0, text1)
 }
